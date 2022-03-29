0e6cb9700980f95a4328916c5f4dac4184aa8cc8
James <jamesthao32@gmail.com>
2022-03-28 19:19:35 -0600

    commit tree 82abad02676583a75c3598f2b3136ab0cb033dc7

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
-// Your code here
+function moveDodgerLeft(dodger = document.getelementbyid) {
+    const leftNumbers = doger.style.left.replace("px", ""); 
+    const left = parseInt(leftNumbers, 10)
+
+    if (left > 0) {
+        doger.style.left = `${left +- 1}px`;
+    }
+}
+
+function moveDodgerRight(dodger = document.getelementbyid) {
+    const rightNumbers = doger.style.right.replace("px", ""); 
+    const right = parseInt(rightNumbers, 10);
+    if (right > 0) {
+        doger.style.right = `${right -1}px`;
+
+    }
+}
+
